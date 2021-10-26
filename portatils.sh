3667c56c326f6ba6d35e91ce990bec688dc0fe9e
chverma <me@chverma.com>
2021-10-26 10:56:15 +0200

    Add script for update educa notebooks

---
+++ portatils.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+llx-guest-manager enable
+
+
+SYSTEM_CONN=/etc/NetworkManager/system-connections/
+OLD_SSID=MYSSID
+OLD_PASSWD=MYPASSWD
+OLD_UUID=MYUUID
+
+NEWSSID=REPLACE
+NEWPASSWD=REPLACE
+DEVICE=wlan0
+NEWUUID=$(nmcli -t -f device,uuid connection | grep $DEVICE | cut -d":" -f2)
+
+SERVER_HOST=REPLACE
+SERVER_PORT=1234
+sudo wget -O $SYSTEM_CONN$NEWSSID https://raw.githubusercontent.com/chverma/lliurex-scripts/master/template_wifi
+
+sudo chmod 600 $SYSTEM_CONN$NEWSSID
+
+sudo sed -i  -e "s/$OLD_SSID/$NEWSSID/g" -e "s/$OLD_PASSWD/$NEWPASSWD/g" -e "s/$OLD_UUID/$NEWUUID/g" $SYSTEM_CONN$NEWSSID
+
+cat /sys/class/net/$DEVICE/address | nc -q1 $SERVER_HOST $SERVER_PORT && sudo reboot
